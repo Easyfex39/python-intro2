774bbd2b4eda2d1d4bb38dc62445aac9a186a510
easyfex <idahosaefosa19@gmail.com>
2024-11-10 08:11:02 -0500

    my python intro2

---
+++ userinput.py
@@ -0,0 +1,14 @@
+# python code that will collect my information
+print("enter my surname ")
+surname=input()
+print("enter your first name")
+firstname=input()
+print("enter my phone number")
+phoneno=input()
+print("details" , surname , firstname ,phoneno)
+
+# write a python code that calculate the of a rectangle of length 10 and breadth meter
+1-10
+b=8
+area-1*b
+print(area)

1961669f875f1a77fb854a397195af7be4518a95
easyfex <idahosaefosa19@gmail.com>
2024-11-17 08:15:59 -0500

    account.py

---
+++ condition.py
@@ -0,0 +1,15 @@
+a=50
+b=100
+c=250
+f=a+b+c
+print(f)
+if f>200:
+    print("its greater than 200")
+    total=f/3
+    print(total)
+else:
+    print("its less than 200")
+    total=f*2000
+    print(total)
+    
+        
